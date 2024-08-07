d24f2cd287d7b80af970acd9b8df1b834298cc06
DanielZhu123 <168749471+DanielZhu123@users.noreply.github.com>
2024-08-08 07:28:50 +0800

    Update tt_um_ran_DanielZhu.sv

--- src/tt_um_ran_DanielZhu.sv
+++ src/tt_um_ran_DanielZhu.sv
@@ -1,16 +1,37 @@
-
 `default_nettype none
 
-module tt_um_ran_DanielZhu
-(
-        input wire clk,
-        input wire rst_n,
-        input wire startring,
-        input wire pulse,
-	input wire sample,
-        input wire diplaychoose,
-	output wire [13:0] displaypin
+
+module tt_um_ran_DanielZhu (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
+
+
+  assign ui_in[0] =startring ; 
+  assign ui_in[1] =pulse; 
+  assign ui_in[2] =sample ; 
+  assign ui_in[3] =diplaychoose ; 
+  assign uo_[7:4]= 4'b0000;
+  assign uio_out = 0;
+  assign uio_oe  = 8'b11111111;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+
+
+
+        logic startring;
+        logic pulse;
+	logic sample;
+        logic diplaychoose;
+	logic [13:0] displaypin;
+
 
 
 logic inverterringout;

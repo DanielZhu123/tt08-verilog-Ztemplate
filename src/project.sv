401f01d2912741fa13e29cb58ad5ea49ea52b26f
DanielZhu123 <168749471+DanielZhu123@users.noreply.github.com>
2024-09-03 15:21:08 +0800

    Update project.sv

--- src/project.sv
+++ src/project.sv
@@ -307,8 +307,8 @@ module tt_13n #(
 	output wire ran13nout);
 
 	logic [count:0] connection;//all of the wire required in the connection
-	always@(posedge clk)begin//pass down bit each clk 
-		if (rst_n==0) begin
+	always@(posedge clk or negedge rst_n)begin//pass down bit each clk 
+		if (!rst_n) begin
 			connection[13:0]<=14'b0;
 		end
 		else begin
@@ -360,8 +360,8 @@ module tt_process (
 		.num(bitsadjacent[2]),
         .ran13nout(bitaft13n));
 
-    always @(posedge clk)begin//prepare for grouping
-    if (rst_n==0) begin
+    always @(posedge clk or negedge rst_n)begin//prepare for grouping
+	    if (!rst_n) begin
 	    bitsadjacent[0]<=0;
 	    bitsadjacent[1]<=0;
 	    bitsadjacent[2]<=0;
